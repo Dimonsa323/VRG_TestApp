09893b9d6c7311bdb557a4178ca29cd2959c650d
Dima Hubenia <dima96gubenya@gmail.com>
2025-11-12 15:12:04 +0200

    Final commits

---
+++ VRG_TestAppTests/VRG_TestAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  VRG_TestAppTests.swift
+//  VRG_TestAppTests
+//
+//  Created by Дима Губеня on 07.11.2025.
+//
+
+import Testing
+@testable import VRG_TestApp
+
+struct VRG_TestAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
